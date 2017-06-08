3dced764af80c45a42696f5ffbfc41baa6b7a427
kira1928 <kira1928@gmail.com>
2017-06-08 07:54:16 -0700

    fix example douyu room id

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
 with ydl:
 	result = ydl.extract_info(
-		"http://douyu.com/lpl",
+		"http://douyu.com/288016", # LPL
 		download=False
 	)
 
